a6bba126b2eb621c33ebc19f9b340fa5dbb156a1
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-25 13:40:10 -0300

    video 7 operador suma con listas

--- practicaLista.py
+++ practicaLista.py
@@ -1,4 +1,4 @@
 miLista = ["María", 5, True, 78.5]
-miLista.extend(["Sandra", "Ana", "Lucía"])
-miLista.pop()
-print(miLista[:])
+miLista2 =["Sandra","Lucía"]
+miLista3=miLista+miLista2
+print(miLista3)
