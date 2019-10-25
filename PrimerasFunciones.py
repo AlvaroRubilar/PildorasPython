9dcdff5b378a8e13518b7fc7deb8642fa700a033
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-25 00:23:06 -0300

    video 5 repetir función print

--- PrimerasFunciones.py
+++ PrimerasFunciones.py
@@ -1,3 +1,19 @@
 print("Estamos aprendiendo Python")
 print("Estamos aprendiendo instrucciones básicas")
 print("Paso a iremos avanzando ")
+
+print("Estamos aprendiendo Python")
+print("Estamos aprendiendo instrucciones básicas")
+print("Paso a iremos avanzando ")
+
+print("Estamos aprendiendo Python")
+print("Estamos aprendiendo instrucciones básicas")
+print("Paso a iremos avanzando ")
+
+print("Estamos aprendiendo Python")
+print("Estamos aprendiendo instrucciones básicas")
+print("Paso a iremos avanzando ")
+
+print("Estamos aprendiendo Python")
+print("Estamos aprendiendo instrucciones básicas")
+print("Paso a iremos avanzando ")
