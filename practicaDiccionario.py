d8dc4c9eba60649fa47dec5ace0deefa1073e6e0
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-25 19:42:46 -0300

    video 9 métodos keys y values

--- practicaDiccionario.py
+++ practicaDiccionario.py
@@ -1,2 +1,4 @@
 miDiccionario = {23:"Jordan","Nombre":"Michael","Equipo":"Chicago","Anillos":{"temporadas":[1991,1992,1993,1996,1997,1998]}}
-print(miDiccionario)
+print(miDiccionario.keys())
+print(miDiccionario.values())
+print(len(miDiccionario))
