31d904d3973226d8bea02747e904e6882be58e52
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-25 01:16:53 -0300

    video 6 uso return

--- PrimerasFunciones.py
+++ PrimerasFunciones.py
@@ -1,10 +1,8 @@
-def suma(num1,num2):
-    
-    print(num1+num2)
-
-suma(5,7)
-suma(2,3)
-suma(35,358)
-    
+def suma(num1, num2):
+    resultado = num1+num2
+    return resultado
 
 
+print(suma(5, 7))
+print(suma(2, 3))
+print(suma(35, 358))
