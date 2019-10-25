29ae89af034b12ef903734bafc9c06d4ae4cedaa
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-25 19:40:09 -0300

    video 9 diccionario dentro de otro diccionario

--- practicaDiccionario.py
+++ practicaDiccionario.py
@@ -1,2 +1,2 @@
-miDiccionario = {23:"Jordan","Nombre":"Michael","Equipo":"Chicago","Anillos":[1991,1992,1993,1996,1997,1998]}
+miDiccionario = {23:"Jordan","Nombre":"Michael","Equipo":"Chicago","Anillos":{"temporadas":[1991,1992,1993,1996,1997,1998]}}
 print(miDiccionario["Anillos"])
