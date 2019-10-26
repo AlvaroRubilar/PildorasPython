1768e232b5dbce62ee11b6bffaf213d698722b39
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-25 22:32:46 -0300

    video 10 error input

--- practicaIF.py
+++ practicaIF.py
@@ -1,6 +1,10 @@
+print("programa de evaluación de notas de alumnos")
+notaAlumno = input()
+
 
 def evaluacion(nota):
-    valoracion="aprobado" if nota>5 else "suspenso"
+    valoracion = "aprobado" if nota >= 5 else "suspenso"
     return valoracion
 
-print(evaluacion(4))
+
+print(evaluacion(notaAlumno))
