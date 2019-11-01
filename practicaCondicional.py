d0241d38634b39c9f70a669fcd982fb96dcbc0ac
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-31 21:12:00 -0300

    video 13 operador in

--- practicaCondicional.py
+++ practicaCondicional.py
@@ -1,13 +1,5 @@
-print("Programa de becas año 2019")
+print("Asignaturas optativas año 2019")
+print("Asinaturas Optativas: Informática gráfica - Pruebas de software - Usabilidad y accesibilidad")
+asignatura=input("Escribe la escogida: ")
 
-distanciaEscuela = int(input("Introduce la distancia a la escuela en km "))
-print(distanciaEscuela)
-
-numeroHermanos = int(input("Introduce el n° de hermanos en el centro "))
-print(numeroHermanos)
-
-salarioFamiliar = int(input("Introduce salario anual en bruto "))
-print(salarioFamiliar)
-
-print("Tienes derecho a beca" if distanciaEscuela > 40 and numeroHermanos >
-      2 or salarioFamiliar <= 20000 else "No tienes derecho a beca")
+print("Asignatura elegida "+asignatura if asignatura in ("Informática gráfica","Informática gráfica", "Usabilidad y accesibilidad") else "La asignatura no está contemplada")
