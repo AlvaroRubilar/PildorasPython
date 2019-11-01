5cd1bdca597267edca279418a298632428e9e065
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-31 21:41:29 -0300

    video 13

--- practicaCondicional.py
+++ practicaCondicional.py
@@ -1,5 +1,5 @@
 print("Asignaturas optativas año 2019")
 print("Asinaturas Optativas: Informática gráfica - Pruebas de software - Usabilidad y accesibilidad")
-asignatura=input("Escribe la escogida: ")
+asignatura=input("Escribe la escogida: ").lower()
 
-print("Asignatura elegida "+asignatura if asignatura in ("Informática gráfica","Informática gráfica", "Usabilidad y accesibilidad") else "La asignatura no está contemplada")
+print("Asignatura elegida " + asignatura.capitalize() if asignatura in ("Informática gráfica".lower(),"Pruebas de software".lower(), "Usabilidad y accesibilidad".lower()) else "La asignatura no está contemplada")
