55186ee9627830502bb35e75199c32178b589632
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-11-06 12:11:04 -0300

    video 15 comprobar email

--- PracticaFor.py
+++ PracticaFor.py
@@ -1,2 +1,5 @@
-for i in "pildorasinformaticas.es":
-    print("Hola", end=" ")
+email = False
+for i in "juan@pildorasinformaticas.es":
+    if (i == "@"):
+        email = True
+print("El email es correcto"if email == True else "El email no es correcto")
