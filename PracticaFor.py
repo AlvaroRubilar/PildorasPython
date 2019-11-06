27a60ab835137a9eb2c2d1e0fdc14c49b6f74da2
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-11-06 12:24:13 -0300

    video 15 for usando range

--- PracticaFor.py
+++ PracticaFor.py
@@ -1,7 +1,2 @@
-contador = 0
-
-miEmail = input("Introduce tu dirección de email: ")
-for i in miEmail:
-    if (i == "@" or i == "."):
-        contador = contador + 1
-print("El email es correcto"if contador >= 2 else "El email no es correcto")
+for i in range(5):
+    print("Hola")
