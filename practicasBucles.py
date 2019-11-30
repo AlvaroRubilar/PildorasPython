dff2012445ec818a9e4ea464dc4273e5b59dfd4d
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-11-30 13:12:05 -0300

    video 18 else en for se activa cuando termina

--- practicasBucles.py
+++ practicasBucles.py
@@ -1,2 +1,8 @@
-class MiClase:
-    pass
+email= input("Introduce tu email, por favor: ")
+for i in email:
+    if i=="@":
+        arroba=True
+        break
+else:
+    arroba=False
+print(arroba)
