8a9a3acdf9c61f17886b97ee07dae75c6beb2431
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-11-30 12:14:57 -0300

    Ingresar edad positiva

--- practicaWhile.py
+++ practicaWhile.py
@@ -1,6 +1,6 @@
-i=1
-
-while i<=10:
-    print("Ejecución "+ str(i))
-    i+=1
-print("terminó la ejecución")
+edad = int(input("Introduce tu edad por favor: "))
+while edad < 0:
+    print("Has introducido una edad negativa. Vuelve a intentarlo")
+    edad = int(input("Introduce tu edad por favor: "))
+print("Gracias por colaborar. Puedes pasar")
+print("Edad del aspirante "+ str(edad))
