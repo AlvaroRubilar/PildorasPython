ba3c6e72362eb33715215216c741ba4543685ff9
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-14 22:41:04 -0300

    video 27 cambios en los métodos

--- poo.py
+++ poo.py
@@ -4,24 +4,26 @@ class Coche():
     ruedas = 4
     enMarcha = False
 
-    def arrancar(self):
-        self.enMarcha = True
-
-    def estado(self):
+    def arrancar(self, arrancamos):
+        self.enMarcha = arrancamos
         if (self.enMarcha):
             return "El Coche está en marcha"
         else:
             return "El Coche está parado"
 
+    def estado(self):
+        print("El coche tiene ", self.ruedas, " ruedas. Un ancho de ", self.anchoChasis, " y un largo de ",
+              self.largoChasis)
+
 
 miCoche = Coche()
 print("El largo del coche es: ", miCoche.largoChasis)
 print("El coche tiene", miCoche.ruedas, " ruedas")
-miCoche.arrancar()
-print(miCoche.estado())
+print(miCoche.arrancar(True))
+miCoche.estado()
 print("--------------------A contiuaación creamos el segundo objeto----------------------")
 miCoche2 = Coche()
 print("El largo del coche es: ", miCoche2.largoChasis)
 print("El coche tiene", miCoche2.ruedas, " ruedas")
-
-print(miCoche2.estado())
+print(miCoche2.arrancar(False))
+miCoche2.estado()
