2bcbad5c5464c82c2ea232155b69d8aae74934aa
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-14 22:57:52 -0300

    video 27 encapsulación

--- poo.py
+++ poo.py
@@ -1,20 +1,20 @@
 class Coche():
     def __init__(self):
-        self.largoChasis = 250
-        self.anchoChasis = 120
-        self.ruedas = 4
-        self.enMarcha = False
+        self.__largoChasis = 250
+        self.__anchoChasis = 120
+        self.__ruedas = 4
+        self.__enMarcha = False
 
     def arrancar(self, arrancamos):
-        self.enMarcha = arrancamos
-        if (self.enMarcha):
+        self.__enMarcha = arrancamos
+        if (self.__enMarcha):
             return "El Coche está en marcha"
         else:
             return "El Coche está parado"
 
     def estado(self):
-        print("El coche tiene ", self.ruedas, " ruedas. Un ancho de ", self.anchoChasis, " y un largo de ",
-              self.largoChasis)
+        print("El coche tiene ", self.__ruedas, " ruedas. Un ancho de ", self.__anchoChasis, " y un largo de ",
+              self.__largoChasis)
 
 
 miCoche = Coche()
@@ -25,5 +25,5 @@ print("--------------------A contiuaación creamos el segundo objeto------------
 miCoche2 = Coche()
 
 print(miCoche2.arrancar(False))
-miCoche2.ruedas = 5
+miCoche2.__ruedas = 5
 miCoche2.estado()
