164a78c895686d192ee795e4289ac0efde9597a2
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-24 01:03:49 -0300

    video 35 creacion paquete calculos

---
+++ calculos/calculosGenerales.py
@@ -0,0 +1,22 @@
+def sumar(op1, op2):
+    print("El resultado de la suma es: ", op1 + op2)
+
+
+def restar(op1, op2):
+    print("El resultado de la resta es: ", op1 - op2)
+
+
+def multiplicar(op1, op2):
+    print("El resultado de la multiplicación es: ", op1 * op2)
+
+
+def potencia(base, exponente):
+    print("El resultado de la potencia es:", base ** exponente)
+
+
+def division(dividendo, divisor):
+    print("El resultado de la división es: ", dividendo / divisor)
+
+
+def redondeo(numero):
+    print("El resultado al redondear es: ", round(numero))
