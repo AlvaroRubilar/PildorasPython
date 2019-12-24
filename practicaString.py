291b636ccad8c0c853a5afc4e3517c1e8b79d594
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-23 23:14:55 -0300

    video 33 while

--- practicaString.py
+++ practicaString.py
@@ -1,3 +1,5 @@
-edad=input("Introduce tu edad: ")
-print(edad.isdigit())
-
+edad = input("Introduce tu edad: ")
+while(edad.isdigit()==False):
+    print("Por favor, introduce un valor numérico: ")
+    edad = input("Introduce tu edad: ")
+print("No puedes pasar" if int(edad) < 18 else "Puedes pasar")
