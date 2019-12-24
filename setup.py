76156a070b0b419b7a1e0cb5fbfa6ba266f66c88
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-24 01:32:40 -0300

    video 36 creacion de paquete distribución

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="paqueteCalculos",
+    version="1.0",
+    description="Paquete de redondeo y potncia",
+    author="Alvaro",
+    author_email="alvaro.rubilar@usach.cl",
+    packages=["calculos","calculos.redondeoPotencias"]
+)
