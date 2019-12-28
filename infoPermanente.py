25167fc3eda1244d64434554f96785257347d387
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-28 10:38:04 -0300

    video 41 creación clase listaPersonas

--- infoPermanente.py
+++ infoPermanente.py
@@ -9,6 +9,21 @@ class Persona:
         print("Se ha creado una persona nueva con el nombre de ",self.nombre)
     def __str__(self):
         return "{} {} {}".format(self.nombre,self.genero,self.edad)
-p = Persona("Sandra","Femenino",29)
 
+
+class ListaPersonas:
+    personas =[]
+    def agregarPersonas(self,p):
+        self.personas.append(p)
+    def mostrarPersonas(self):
+        for p in self.personas:
+            print(p)
+miLista =ListaPersonas()
+p = Persona("Sandra","Femenino",29)
+miLista.agregarPersonas(p)
+p = Persona("Antonio","Masculino",39)
+miLista.agregarPersonas(p)
+p = Persona("Ana","Femenino",19)
+miLista.agregarPersonas(p)
+miLista.mostrarPersonas()
 
