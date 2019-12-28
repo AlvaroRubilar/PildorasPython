e07e1cd7ffce5a5a05e682f6bbfea70722c1d8b0
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-27 23:20:17 -0300

    video 38 sobrescribir linea 1

--- manejoArchivos.py
+++ manejoArchivos.py
@@ -2,6 +2,12 @@ from io import open
 
 archivoTexto = open("archivo.txt", "r+")
 
-archivoTexto.write("Comienzo del texto ")
+#print(archivoTexto.readlines())
+lineasTexto = archivoTexto.readlines()
+lineasTexto[1]=" Esta línea ha sido incluida desde el exterior\n"
+archivoTexto.seek(0)
+archivoTexto.writelines(lineasTexto)
+archivoTexto.close()
+
 
 
