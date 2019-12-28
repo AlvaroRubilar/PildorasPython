0521bc85bd22b667d33bab0cf1856077cb867ca3
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-28 00:19:52 -0300

    video 39 leer archivo binario

--- trabajoSerializacion.py
+++ trabajoSerializacion.py
@@ -1,6 +1,6 @@
 import pickle
 
-listaNombres = ["Pedro", "Ana", "María", "Isabel"]
-ficheroBinario = open("listaNombres","wb")
-pickle.dump(listaNombres,ficheroBinario)
-ficheroBinario.close()
+
+fichero = open("listaNombres","rb")
+lista = pickle.load(fichero)
+print(lista)
