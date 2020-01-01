e2f385ab3dbcafedf75a632ed0b55a28af49cfde
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-01 18:23:31 -0300

    video 44 cambio de fuente color y tamaño

--- graficos/practicaLabel.py
+++ graficos/practicaLabel.py
@@ -2,7 +2,7 @@ from tkinter import *
 root =Tk()
 miFrame = Frame(root, width=500, height=400)
 miFrame.pack()
-Label(miFrame, text="Hola Alumnos de Python").place(x=100,y=200)
+Label(miFrame, text="Hola Alumnos de Python", fg="#0000ff", font=("Comic Sans MS",18)).place(x=100,y=200)
 
 
 root.mainloop()
