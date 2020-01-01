5e650b9941fcfa8fc4eac99f536237a4adce1859
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-01 18:38:08 -0300

    video 45 ubicarlo dentro de un frame

--- graficos/practicaEntry.py
+++ graficos/practicaEntry.py
@@ -1,5 +1,7 @@
 from tkinter import *
 raiz = Tk()
+miFrame = Frame(raiz, width=1200, height=600)
+miFrame.pack()
 cuadroTexto = Entry(raiz)
-cuadroTexto.pack()
+cuadroTexto.place(x=100, y=100)
 raiz.mainloop()
