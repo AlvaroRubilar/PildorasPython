2fd1f1362dd7624a3ac65ff15d94ad2012c4fd37
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-01 18:20:06 -0300

    video 44 place respeta el tamaño del frame

--- graficos/practicaLabel.py
+++ graficos/practicaLabel.py
@@ -3,6 +3,6 @@ root =Tk()
 miFrame = Frame(root, width=500, height=400)
 miFrame.pack()
 miLabel = Label(miFrame, text="Hola Alumnos de Python")
-miLabel.pack()
+miLabel.place(x=100,y=200)
 
 root.mainloop()
