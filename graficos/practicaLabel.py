cd555848e0cf36ac53eb4847208aad9aca5b7eb6
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-01 18:18:59 -0300

    video 44 insertar texto

---
+++ graficos/practicaLabel.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root =Tk()
+miFrame = Frame(root, width=500, height=400)
+miFrame.pack()
+miLabel = Label(miFrame, text="Hola Alumnos de Python")
+miLabel.pack()
+
+root.mainloop()
