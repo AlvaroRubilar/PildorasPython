418e8c23e05a3f71728617c6e3e29db47b365248
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2020-01-01 16:43:12 -0300

    video 42 color de fondo

--- graficos/primeraInterface.py
+++ graficos/primeraInterface.py
@@ -2,7 +2,8 @@ from tkinter import *
 
 raiz = Tk()
 raiz.title("Ventana de pruebas")
-raiz.resizable(True,0)
+raiz.resizable(True, 0)
 raiz.iconbitmap("retriever.ico")
 raiz.geometry("650x350")
+raiz.config(bg = "salmon")
 raiz.mainloop()
